7c886e66f94a9697e1b99da5d7732f3d3a4e7811
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-01-01 12:45:47 +0530

    Remove Analytics as it doesn't work

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
-import { Analytics } from "@vercel/analytics/react";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
