36ce14ecfe25f5f8775834157d4c20832de9e8e1
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-06-17 19:01:32 +0530

    config driven

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { Projects } from '../components/Projects';
 import { Footer } from '../components/Footer';
 import { POR } from '../components/POR';
 import { Awards } from '../components/Awards';
+import ExperienceTimelineData from '../configs/experience_timeline.json';
 
 export default function Home() {
   return (
@@ -22,44 +23,7 @@ export default function Home() {
           <AboutMe />
         </div>
         <div className="w-full mt-8">
-          <TimeLine steps={[
-            {
-              dates: "Mar 2022 - Current", position: 'SWE-2', company: 'OPG, E+D, Microsoft IDC', description: [
-                "Working on new initiatives in the Productivity Innovations & Incubation team in the Office AI division.",
-                "Rapid prototyping and new product development with various emerging technologies.",
-                "Partnering with GitHub Next to take my hackathon innovation Hello Cody to market via the GitHub Copilot Voice project."
-              ], skills: ["TypeScript", "NodeJS", "ReactJS", "Open AI", "LLM", "Prompt Engineering", "Python"]
-            },
-            {
-              dates: "July 2019 - Current", position: 'SWE-1', company: 'C+E, C+AI, Microsoft IDC', description: [
-                "Worked as a full-stack developer with a front-end concentration in the Azure Commerce+Ecosystems group in Azure Cloud+AI.",
-                "Designed and helped develop some of the most critical tools and apps used by Microsoft sellers everyday."
-              ], skills: ["PySpark", "Databricks", "React.js", "Typescript"]
-            },
-            {
-              dates: "July 2019 - Current", position: 'SWE-1', company: 'C+E, C+AI, Microsoft IDC', description: [
-                "Worked as a full-stack developer with a front-end concentration in the Azure Commerce+Ecosystems group in Azure Cloud+AI.",
-                "Designed and helped develop some of the most critical tools and apps used by Microsoft sellers everyday."
-              ], skills: ["PySpark", "Databricks", "React.js", "Typescript"]
-            },
-            {
-              dates: "Oct 2018 - June 2019", position: 'SWE (part-time)', company: 'VoiceQube', description: [
-                "Part-time role where I developed consumer-grade Alexa Skills and Google Assistant actions for clients.",
-                "Also developed end-to-end solutions for clients around managing app content, integrating Alexa with Raspberry Pi, etc."
-              ], skills: ["Node.js", "DynamoDB", "AWS Lambda"]
-            },
-            {
-              dates: "Summer 2018", position: 'Summer Intern', company: 'Voycefirst', description: [
-                "Ideated, developed, and deployed several consumer-facing Amazon Alexa Skills using Node.js, DynamoDB, and AWS Lambda.",
-                "One of the skills developed, Trivia Nation, won the Skill of the Month award for the best game skill in a monthly contest organized by Amazon at the time."
-              ], skills: ["Node.js", "DynamoDB", "AWS Lambda"]
-            },
-            {
-              dates: "Summer 2017", position: 'Summer Intern', company: 'IIIT Hyderabad', description: [
-                "Interned at the Lab for Spatial Informatics where I worked on building a GUI application to convert 2D CAD drawing models into GIS ready data models."
-              ], skills: ["GUI development", "CAD", "GIS"]
-            }
-          ]} />
+          <TimeLine steps={ExperienceTimelineData} />
         </div>
         <div className="mt-8">
           <SkillsMap />
