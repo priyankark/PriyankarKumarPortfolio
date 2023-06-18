1cae3194bbc7188f5dc612ccc122cc99e2886029
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-06-18 17:22:41 +0530

    Footer improvement

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart} from '@fortawesome/free-solid-svg-icons';
-import { faReact} from '@fortawesome/free-brands-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faReact } from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
-    return <footer className="flex flex-col bg-display-100 p-8 z-10 shadow-inner">
-        <div className="mx-auto">
-            {`Designed & Developed by Priyankar Kumar`}
-        </div>
-        <div className="mx-auto">
-            <span> Built with <FontAwesomeIcon icon={faHeart}/> using  <FontAwesomeIcon icon={faReact}/> , Next.js and Tailwind CSS </span>
-        </div>
+  return (
+    <footer className="flex flex-col bg-gray-200 p-8 z-10 shadow-md">
+      <div className="mx-auto text-center">
+        <p>
+          Designed & Developed by Priyankar Kumar
+        </p>
+        <p className="mt-2">
+          Built with
+          <FontAwesomeIcon icon={faHeart} className="mx-1 text-red-500" />
+          using
+          <FontAwesomeIcon icon={faReact} className="mx-1 text-blue-500" />,
+          Next.js, and Tailwind CSS
+        </p>
+        <p className="mt-2">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded inline-flex items-center"
+            style={{backgroundColor: "black"}}
+          >
+            <FontAwesomeIcon icon={faGithub} className="mr-2" />
+            Clone my portfolio
+          </button>
+        </p>
+      </div>
     </footer>
-}
+  );
+};
